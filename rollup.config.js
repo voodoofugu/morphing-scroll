4b7477ab5346e321dc3fa5276ea97755652f74f8
voodoofugu <schilin.georg@gmail.com>
2024-09-18 19:29:21 +0300

    rename & packages separation

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,21 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import del from "rollup-plugin-delete";
 
-export default {
-  input: "./src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "esm",
+        globals: {
+          react: "React",
+        },
       },
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
-      },
-    },
-  ],
-  plugins: [resolve(), commonjs(), typescript(), terser()],
-  external: ["react", "react-dom"],
-};
+    ],
+    plugins: [del({ targets: "dist/*" }), resolve(), typescript(), terser()],
+    external: ["react"],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,4 @@ import IntersectionTracker from "./IntersectionTracker";
 import ResizeTracker from "./ResizeTracker";
 import Scroll from "./Scroll";
 
-const RVS = {
-  Scroll,
-  ResizeTracker,
-  IntersectionTracker,
-};
-
-export default RVS;
 export { Scroll, ResizeTracker, IntersectionTracker };
